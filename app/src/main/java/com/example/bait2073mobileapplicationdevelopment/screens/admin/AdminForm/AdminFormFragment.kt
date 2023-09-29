7af679450659557f509a1532d03cd0b0a33b1ed1
foyo-maker <foyotee@Gmail.com>
2023-09-29 12:09:01 +0800

    commit for better environment

--- app/src/main/java/com/example/bait2073mobileapplicationdevelopment/screens/admin/AdminForm/AdminFormFragment.kt
+++ app/src/main/java/com/example/bait2073mobileapplicationdevelopment/screens/admin/AdminForm/AdminFormFragment.kt
@@ -346,7 +346,7 @@ class AdminFormFragment : Fragment() {
             isValidate = false
         }
         if (phoneError != null) {
-            //errr
+            //errrh
             layoutPhone.error = phoneError
             isValidate = false
         }
